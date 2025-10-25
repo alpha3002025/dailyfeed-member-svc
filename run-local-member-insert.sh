718c8061cf02362384cccf2751e89bd957bf02f1
alpha3002025 <alpha300uk@gmail.com>
2025-10-25 16:59:55 +0900

    feat: 블랙리스트,RefreshToken 기능에 Redis 레이어를 통해 최적화

--- run-local-member-insert.sh
+++ run-local-member-insert.sh
@@ -1,4 +1,4 @@
-./gradlew :dailyfeed-member:test --tests "click.dailyfeed.member.domain.member.integratedtest.SignupInsertTest.fixture__insert_sample_data"
+./gradlew :dailyfeed-member:test --tests "click.dailyfeed.member.domain.member.integratedtest.SignupInsertLocalWASTest.fixture__insert_sample_data_with_ai_generated_image" --stacktrace --info
 
 
 ## ./gradlew :dailyfeed-member:test --tests "*.SignupInsertTest.fixture__insert_sample_data"
